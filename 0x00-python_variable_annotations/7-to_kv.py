46d3791f9c5eaa35b0b76e2363e53d69a23b5c2a
root <ucheogbonnak@gmail.com>
2023-01-05 11:12:55 -0800

    added

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 """
-Atype annotated function that returns a tuple
+A type annotated function that returns a tuple
 """
-from typing import List, Union, Tuple
+from typing import Union, Tuple
 
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Returns a tuple
+    Returns a tuple. The first element of the tuple is the string k
+    The second element is the square of the int/float v
+    And should be annotated as a float
     """
     return (k, v * v)
